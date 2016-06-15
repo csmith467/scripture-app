4ef6a1aa1230b9f33fdb9b1914d54e8db8439987
Craig Smith <craigsmith467@gmail.com>
2016-06-14 19:08:30 -0500

    Adding new login screen.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,8 +11,20 @@ angular.module('scriptureApp')
         if (data.length >= 1) {
           $cookies.put("passcode", $scope.passcode);
           $state.go('home');
+        } else {
+          $scope.$parent.showMessage("Incorrect password", "error");
+          $scope.passcode = "";
         }
       })
+    }
+
+    $scope.selectNumber = function(num) {
+      if ($scope.passcode.length < 4) {
+        $scope.passcode += num;
+        if ($scope.passcode.length == 4) {
+          $scope.login();
+        }
+      }
     }
 
   });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -6,4 +6,23 @@ angular.module('scriptureApp')
       $cookies.remove("passcode");
       $state.go("login");
     }
+
+    // Global function to show message bar
+    $scope.showMessage = function(message, type) {
+      // Set message text
+      $("#messageBar").html(message);
+
+      // Set background based on message type
+      if (type == "info") {
+        $("#messageBar").css("background", "#2196F3");
+      } else if (type == "error") {
+        $("#messageBar").css("background", "#F44336");
+      }
+
+      // Show message bar
+      $("#messageBar").addClass('message-bar-show');
+      setTimeout(function() {
+         $("#messageBar").removeClass('message-bar-show');
+      }, 3000);
+    }
   });
